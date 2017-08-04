576535065bff0a8786c8cde247b156bc361eda93
NessCollege <nesscollege2017@gmail.com>
2017-08-04 10:30:16 +0300

    Initial commit

---
+++ app/src/main/java/ness/edu/contactsprovider/ContactsDataSource.java
@@ -0,0 +1,21 @@
+package ness.edu.contactsprovider;
+
+import android.content.Context;
+
+/**
+ * Contacts content provider data
+ * <uses-permission android:name="android.permission.READ_CONTACTS"/>
+
+ */
+
+public class ContactsDataSource {
+
+
+    public static void getContacts(Context context){
+        //Cursor cursor = context.getContentResolver().query();
+    }
+    //Uri.
+    //column names.
+
+    //object to query the uri ContentResolver.
+}
